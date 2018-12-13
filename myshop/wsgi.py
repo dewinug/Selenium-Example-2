dc707e5a0635cf2e0b3c3bc850ec4de6a36464d1
Olawepo Michael <sejuba@gmail.com>
2018-12-13 23:56:46 +0100

    initial commit

--- myshop/wsgi.py
+++ myshop/wsgi.py
@@ -13,5 +13,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myshop.settings")
 from whitenoise import WhiteNoise
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/shop/static')
-application.add_files('/shop/static')
+application = WhiteNoise(application, root='shop/static/')
+application.add_files('shop/static/')
