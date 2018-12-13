15c03df422e0f9615a79d9fc1a6ffd3bdfeceea1
Olawepo Michael <sejuba@gmail.com>
2018-12-14 00:42:12 +0100

    initial commit

--- myshop/settings.py
+++ myshop/settings.py
@@ -160,7 +160,8 @@ CART_SESSION_ID = 'cart'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # CELERY STUFF
-BROKER_URL = 'redis://localhost:6379'
+BROKER_URL = os.environ.get("REDISCLOUD_URL", "django://")
+# BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
