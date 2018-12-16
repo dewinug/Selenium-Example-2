f9b0665ce1497ce879fcf8c8441e6b29cb28bf84
Olawepo Michael <sejuba@gmail.com>
2018-12-16 12:02:05 +0100

    initial commit

--- myshop/settings.py
+++ myshop/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 # Parse database configuration from $DATABASE_URL
-# DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
